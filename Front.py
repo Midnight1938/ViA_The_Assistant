15dd7b8cb6a95f674b2c71f2992c4e04b24d0a0c
Skshm <eminate894567@gmail.com>
2021-07-22 22:21:46 +0530

    Instructions

--- Front.py
+++ Front.py
@@ -1,6 +1,7 @@
 from Backend.My_First_CB import Ask
 from flask import Flask, render_template, request, jsonify
 from Backend.My_First_CB import Ask as BotAsk
+from Backend.My_First_CB import append_interaction_to_chat_log as ChatLog
 
 
 app = Flask(__name__)
@@ -9,11 +10,24 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 @app.route("/get")
-
 #function for the bot response
+
 def get_bot_response():
-    userText = request.args.get('msg')
-    return str(BotAsk(userText))
+    userText = request.args.get('messageText')
+    return str(BotAsk(userText)) ##?  To chatPanel
+
+#def Bot()
+#    incoming_msg = 
+#    chat_log = 
+#
+#    answer = BotAsk(incoming_msg, chat_log)
+#    session['chat_log'] = ChatLog(incoming_msg, answer, chat_log)
+#    if answer is None:
+#        answer = "Hello, I am ViA, your personal Assistant or something"
+#    response = message(answer)
+#    return response.__str__()
+#? Chat_Log Access would go great.
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8888', debug=True)
