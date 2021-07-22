56085b2a4878c60221b3be6960a835e45577c2f8
Skshm <eminate894567@gmail.com>
2021-07-22 12:50:18 +0530

    Basic Interaction Established

--- Bot/My_First_CB.py
+++ Bot/My_First_CB.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 completion = openai.Completion()
 
 name = "Skshm"
@@ -17,7 +17,7 @@ def Ask(question, chat_log=None):
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt_text,
-        temperature=0.8,
+        temperature=5.3,
         max_tokens=150,
         top_p=1,
         frequency_penalty=0,
@@ -32,4 +32,9 @@ def append_interaction_to_chat_log(question, answer, chat_log=None):
         chat_log = session_prompt 
         return f"{chat_log}{restart_sequence} {question}{start_sequence}{answer}"
 
-print(Ask(question, chat_log=None))
+while True:
+    Name = str(input("Whats your Name? "))
+    question = str(input(f"{Name}: "))
+    if question == "BREAK":
+        break
+    print("Via: ",Ask(question, chat_log=None))
