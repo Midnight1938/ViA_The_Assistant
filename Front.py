8e9bbf7d0ad855f2e862b434cc7ac00453606cd4
Skshm <eminate894567@gmail.com>
2021-07-26 17:37:06 +0530

    Trial Change

--- Front.py
+++ Front.py
@@ -14,5 +14,5 @@ def get_bot_response():
     userText = request.args.get('msg')
     return str(BotAsk(userText))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0")
